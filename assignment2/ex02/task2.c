91f5d043a572da89ced82c95a132ba7b8921bfac
prasu <prai93@outlook.com>
2018-09-29 09:25:40 +0200

    separated in separete functions

--- assignment2/ex02/task2.c
+++ assignment2/ex02/task2.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
 
 /* Returns the index of the next prime (non-zero, since we "knock out" non-primes by setting their value to zero) */
 int getNextPrime(int i, int x[]) {
@@ -8,36 +10,51 @@ int getNextPrime(int i, int x[]) {
   return i;
 }
 
-int main(int argc, char* argv[]) {
-  int* primes;
-  int i, j, NPRIMES;
+void displayPrimeNumbers(int primes[],int NPRIMES)
+{
+  for(int i=2; i < NPRIMES; i++) {
+    if ( primes[i] != 0 ) {
+      printf("Prime number: %d\n", primes[i]);
+    }
+  }
+}
 
-  NPRIMES = 24;
-  primes = (int*)malloc(NPRIMES * sizeof(int));
-
+int *init(int primes[],int NPRIMES)
+{
   /* we start at 2 because it is the smallest prime */
   /*list all numbers from 2 to max */
-  for(i=2; i < NPRIMES; i++) {
+  for(int i=2; i < NPRIMES; i++) {
     primes[i] = i;
     printf("%d\n", i);
   }
+return primes;
+}
 
+int *filterPrimes(int primes[],int NPRIMES){
 /*make iterations from 2 to NPRIMES and update counter with next prime number*/
-  for(i=2; i < NPRIMES; i = getNextPrime(i+1, primes)) {
+  for(int i=2; i < NPRIMES; i = getNextPrime(i+1, primes)) {
    /*find multiple i.e j=i*2 and j=j+i and marked that number by setting array[index]=0 */
-    for(j = (i*2); j < NPRIMES; j += i) {
+    for(int j = (i*2); j < NPRIMES; j += i) {
       printf("i: %d, j: %d\n", i, j);
       primes[j] = 0;
 
     }
 
  }
+return primes;
+}
 
-  for(i=2; i < NPRIMES; i++) {
-    if ( primes[i] != 0 ) {
-      printf("Prime number: %d\n", primes[i]);
-    }
-  }
+int main(int argc, char* argv[]) {
+  int* primes;
+  int NPRIMES;
+
+  NPRIMES = 24;
+  primes = (int*)malloc(NPRIMES * sizeof(int));
+
+ primes=init(primes,NPRIMES);
+primes=filterPrimes(primes,NPRIMES);
+displayPrimeNumbers(primes,NPRIMES);
+
 
   return 0;
 }
