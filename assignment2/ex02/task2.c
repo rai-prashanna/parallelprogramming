ae9e19e91d99b003ac97a85177d4be3ed03c4b94
prasu <prai93@outlook.com>
2018-09-29 20:12:12 +0200

    added subtle bug in implementaion

--- assignment2/ex02/task2.c
+++ assignment2/ex02/task2.c
@@ -3,6 +3,8 @@
 #include <pthread.h>
 #include <math.h>
 
+pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
+int NPRIMES=100;
 /* Define a data type for the arguments to pass to the functions of threads. */
 struct thread_arg_t
 {
@@ -24,15 +26,21 @@ struct thread_arg_t
 /* Returns the index of the next prime (non-zero, since we "knock out" non-primes by setting their value to zero) */
 int getNextPrime(int i, int *x)
 {
+    printf("inside getNextPrime");
+    pthread_mutex_lock(&lock);
     while (*(x + i) == 0)
     {
         i++;
     }
+    pthread_mutex_unlock(&lock);
+
     return i;
 }
 
 void displayPrimeNumbers(int *primes,int NPRIMES)
 {
+    printf("display all primes[] \n");
+
     for(int i=2; i < NPRIMES; i++)
     {
         if ( *(primes +i) != 0 )
@@ -49,21 +57,47 @@ int *init(int *primes,int NPRIMES)
     for(int i=2; i < NPRIMES; i++)
     {
         *(primes +i) = i;
-        printf("%d\n", i);
+
     }
+    printf("init of primes[] \n");
     return primes;
 }
+
+//void *filterPrimes(int startingindex,int *primes,int maxlimit)
+//{
+//    printf("inside filterPrimes()");
+//    /*make iterations from 2 to NPRIMES and update counter with next prime number*/
+//    for(int i=startingindex; i <= maxlimit; i = getNextPrime(i+1, primes))
+//    {
+//        /*find multiple i.e j=i*2 and j=j+i and marked that number by setting array[index]=0 */
+//        for(int j = (i*i); j < maxlimit; j += i)
+//        {
+////            printf("i: %d, j: %d\n", i, j);
+//            if(j%i==0)
+//            {
+//                *(primes +j) = 0;
+//                *(primes +i) = 0;
+//
+//            }
+//
+//        }
+//
+//    }
+//    printf("exited filterPrimes()");
+//    return NULL;
+//}
 
 void *filterPrimes(int startingindex,int *primes,int maxlimit)
 {
     /*make iterations from 2 to NPRIMES and update counter with next prime number*/
+    startingindex=getNextPrime(startingindex, primes);
     for(int i=startingindex; i <= maxlimit; i = getNextPrime(i+1, primes))
     {
         /*find multiple i.e j=i*2 and j=j+i and marked that number by setting array[index]=0 */
-        for(int j = (i*2); j < maxlimit; j += i)
+        for(int j = (i*2); j <= NPRIMES; j += i)
         {
             printf("i: %d, j: %d\n", i, j);
-            primes[j] = 0;
+            *(primes +j) = 0;
 
         }
 
@@ -73,24 +107,24 @@ void *filterPrimes(int startingindex,int *primes,int maxlimit)
 
 void *calculate_prime_pthread_runnable(void *arg)
 {
+    printf("inside runnable()");
     /* Cast the argument from void* to its proper type. */
     struct thread_arg_t *targ = arg;
     /* Add index to seed to make sure that different threads get different
      * seeds even if they are created so close in time that time() returns
      * the same value. */
-    int*  resultprimes,tempprimes;
     unsigned int startingindex =  targ->startindex;
     unsigned int upperlimit =  targ->upperlimit;
-    tempprimes= targ->primes;
+
     filterPrimes(startingindex,targ->primes,upperlimit);
 
+    printf("exited runnable()");
     return NULL;
 }
 
 int main(int argc, char* argv[])
 {
     int* primes;
-    int NPRIMES;
     int NUM_THREADS=1;
     NPRIMES = 100;
     pthread_t threads[NUM_THREADS]; /* An array of the threads. */
@@ -100,36 +134,36 @@ int main(int argc, char* argv[])
 
     primes=init(primes,NPRIMES);
     filterPrimes(2,primes,sqrt_num);
-
-    /* Create (and run) all the threads. */
-
-    primes=filterPrimes(2,primes,sqrt_num);
-
-    for (int i = sqrt_num+1,j=0; j < NUM_THREADS; j++)
-    {
-        if(i>=NPRIMES)
-        {
-            break;
-        }
-        args[j].startindex = i;
-        args[j].upperlimit=NPRIMES;
-        args[j].primes=primes;
-        if (pthread_create(&threads[j], NULL,calculate_prime_pthread_runnable, &args[j]))
-        {
-            fprintf(stderr, "Error creating thread #%d!\n", j);
-            exit(1);
-        }
-        i=i+1;
-
-    }
-
-    /* Block until all threads are finished. */
-    for (int i = 0; i < NUM_THREADS; i++)
-    {
-        pthread_join(threads[i], NULL);
-    }
-
-//displayPrimeNumbers(primes,NPRIMES);
+    filterPrimes(sqrt_num+1,primes,NPRIMES);
+//    /* Create (and run) all the threads. */
+//args[0].primes=primes;
+//    for (int i = sqrt_num+1,j=0; j < NUM_THREADS; j++)
+//    {
+//        if(i>=NPRIMES)
+//        {
+//            break;
+//        }
+//        args[j].startindex = i;
+//        args[j].upperlimit=NPRIMES;
+//
+//        if (pthread_create(&threads[j], NULL,calculate_prime_pthread_runnable, &args[j]))
+//        {
+//            fprintf(stderr, "Error creating thread #%d!\n", j);
+//            exit(1);
+//        }
+//        i=i+1;
+//
+//    }
+//
+//    /* Block until all threads are finished. */
+//    for (int i = 0; i < NUM_THREADS; i++)
+//    {
+//        pthread_join(threads[i], NULL);
+//    }
+//
+//    sleep();
+//    primes=args[0].primes;
+////displayPrimeNumbers(primes,NPRIMES);
 
     /* displayPrimeNumbers(primes,NPRIMES);
     */
