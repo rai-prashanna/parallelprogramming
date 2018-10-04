9dbd55974a5c0a27d9d1aecceffff400ea166b54
prasu <prai93@outlook.com>
2018-10-04 06:48:50 +0200

    refactoring

--- assignment2/ex02/ex2.c
+++ assignment2/ex02/ex2.c
@@ -2,29 +2,41 @@
 #include <stdlib.h>
 #include <pthread.h>
 #include <math.h>
+#include <sys/time.h>
+typedef unsigned long long timestamp_t;
+
+static timestamp_t
+    get_timestamp ()
+    {
+      struct timeval now;
+      gettimeofday (&now, NULL);
+      return  now.tv_usec + (timestamp_t)now.tv_sec * 1000000;
+    }
+
+
 
 pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
-int NPRIMES=100;
+long int NPRIMES=100000000;
 /* Define a data type for the arguments to pass to the functions of threads. */
 struct thread_arg_t
 {
     /* thread id */
-    unsigned int id;
+    unsigned long id;
 
     /* starting index */
-    unsigned int startindex;
+    unsigned long startindex;
 
     /* upper index */
-    unsigned int upperlimit;
+    unsigned long upperlimit;
 
     /* calclauted primes */
-    int* primes;
+    long* primes;
 
 
 };
 
 /* Returns the index of the next prime (non-zero, since we "knock out" non-primes by setting their value to zero) */
-int getNextPrime(int i, int *x)
+long getNextPrime(long i, long *x)
 {
     pthread_mutex_lock(&lock);
     while (*(x + i) == 0)
@@ -36,24 +48,24 @@ int getNextPrime(int i, int *x)
     return i;
 }
 
-void displayPrimeNumbers(int *primes,int NPRIMES)
+void displayPrimeNumbers(long *primes,long NPRIMES)
 {
     printf("display all primes[] \n");
 
-    for(int i=2; i < NPRIMES; i++)
+    for(long i=2; i < NPRIMES; i++)
     {
         if ( *(primes +i) != 0 )
         {
-            printf("Prime number: %d\n", *(primes +i));
+            printf("Prime number: %ld\n", *(primes +i));
         }
     }
 }
 
-int *init(int *primes,int NPRIMES)
+long *init(long *primes,long NPRIMES)
 {
     /* we start at 2 because it is the smallest prime */
     /*list all numbers from 2 to max */
-    for(int i=2; i < NPRIMES; i++)
+    for(long i=2; i < NPRIMES; i++)
     {
         *(primes +i) = i;
 
@@ -63,14 +75,14 @@ int *init(int *primes,int NPRIMES)
 }
 
 
-void *filterPrimes(int startingindex,int *primes,int maxlimit)
+void *filterPrimes(long startingindex,long *primes,long maxlimit)
 {
     /*make iterations from 2 to NPRIMES and update counter with next prime number*/
     startingindex=getNextPrime(startingindex, primes);
-    for(int i=startingindex; i <= maxlimit; i = getNextPrime(i+1, primes))
+    for(long i=startingindex; i <= maxlimit; i = getNextPrime(i+1, primes))
     {
         /*find multiple i.e j=i*2 and j=j+i and marked that number by setting array[index]=0 */
-        for(int j = (i*2); j <= NPRIMES; j += i)
+        for(long j = (i*2); j <= NPRIMES; j += i)
         {
 
             pthread_mutex_lock(&lock);
@@ -90,8 +102,8 @@ void *calculate_prime_pthread_runnable(void *arg)
     /* Add index to seed to make sure that different threads get different
      * seeds even if they are created so close in time that time() returns
      * the same value. */
-    unsigned int startingindex =  targ->startindex;
-    unsigned int upperlimit =  targ->upperlimit;
+    unsigned long startingindex =  targ->startindex;
+    unsigned long upperlimit =  targ->upperlimit;
 
     filterPrimes(startingindex,targ->primes,upperlimit);
 
@@ -100,15 +112,14 @@ void *calculate_prime_pthread_runnable(void *arg)
 
 int main(int argc, char* argv[])
 {
-    int* primes;
+    long* primes;
     int NUM_THREADS=4;
-    NPRIMES = 100000;
     pthread_t threads[NUM_THREADS]; /* An array of the threads. */
     struct thread_arg_t args[NUM_THREADS]; /* One argument struct per thread. */
-    primes = (int*)malloc(NPRIMES * sizeof(int));
-    unsigned int sqrt_num = (int) ceil(sqrt((double) NPRIMES));
-    unsigned int leftnumbers= NPRIMES-sqrt_num;
-    unsigned int equal_intervals=leftnumbers/NUM_THREADS;
+    primes = (long*)malloc(NPRIMES * sizeof(long));
+    unsigned long sqrt_num = (long) ceil(sqrt((double) NPRIMES));
+    unsigned long leftnumbers= NPRIMES-sqrt_num;
+    unsigned long equal_longervals=leftnumbers/NUM_THREADS;
     if( argc == 2 )
     {
 
@@ -121,18 +132,19 @@ int main(int argc, char* argv[])
     }
 
     primes=init(primes,NPRIMES);
-    filterPrimes(2,primes,sqrt_num);
 
+    filterPrimes(2,primes,sqrt_num);
+    timestamp_t t0 = get_timestamp();
     filterPrimes(sqrt_num+1,primes,NPRIMES);
     /* Create (and run) all the threads. */
-    for(int i=0,j=sqrt_num+1; i<NUM_THREADS; i++,j=j+equal_intervals+1)
+    for(long i=0,j=sqrt_num+1; i<NUM_THREADS; i++,j=j+equal_longervals+1)
     {
         args[i].startindex = j;
-        args[i].upperlimit=j+equal_intervals;
+        args[i].upperlimit=j+equal_longervals;
         args[i].primes=primes;
         if(pthread_create(&threads[i], NULL,calculate_prime_pthread_runnable, &args[i]))
         {
-            fprintf(stderr, "Error creating thread #%d!\n", i);
+            fprintf(stderr, "Error creating thread #%ld!\n", i);
             exit(1);
         }
 
@@ -146,7 +158,10 @@ int main(int argc, char* argv[])
 
 
     primes=args[0].primes;
+    timestamp_t t1 = get_timestamp();
+double secs = (t1 - t0) / 1000000.0L;
 
+printf("execution time is   %lf \n",secs );
     displayPrimeNumbers(primes,NPRIMES);
 
     return 0;
