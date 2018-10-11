912152b34bda5378f56420ba30993a9f4de4f6b9
terratrends <johan.alfedeen@gmail.com>
2018-10-11 17:46:27 +0200

    work on tiled seq version

--- assignment3/jdev/ex3-tiled-seq.c
+++ assignment3/jdev/ex3-tiled-seq.c
@@ -1,7 +1,10 @@
-// gcc ex3tiled.c -o ex3tiled
+// A tiled version of FW
+// gcc -Wall ex3tiled.c -o ex3tiled
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <string.h>
+#include <math.h>
 #include <time.h>
 
 typedef struct {
@@ -10,37 +13,32 @@ typedef struct {
     int offset_j;
 } tile;
 
-//#define INIT_NULL_TILE(X) tile X = {.id = -1, .offset_i = -1, .offset_j = -1};
 
 #define CACHE_LINE_SIZE = 64;
 const int intsizebytes = 4;
 // Set tile size to a 4x4 matrix (64/4) = 16 integer values. So 4x4
-const int B_TILE_SIZE = 16;
+const int B_TILE_SIZE = 4;
+//const int B_TILE_SIZE = 16;
 
 const int MAX_RAND = 101; // If 101, we use infinity
 const int INFINITY = 9999;
 int * pInt = NULL;
-tile NULL_TILE = {-1, -1, -1};
 
 int ** initMatrix(int **mtx, int size);
-//void displayMatrix(const int mtx[MATRIX_SIZE][MATRIX_SIZE]);
 void prettyPrintMatrix(int **mtx, int size);
 void floydWarshallTiled(int **mtx, int size);
-void floydWarshallCore(int **mtx, int size, tile t);
+void floydWarshallTileCore(int **mtx, int size, int k);
+//void floydWarshallCore(int **mtx, int size, tile t);
 void floydWarshallStandard(int **mtx, int size);
-tile getEastTile(tile cr, int size);
-tile getWestTile(tile cr, int size);
-tile getNorthTile(tile cr, int size);
-tile getSouthTile(tile cr, int size);
-void printTile(tile cr);
-bool isNullTile(tile t);
+int ** initNextMatrix(int **mtx, int size, int **frommtx);
+void copyMatrixValues(int **frommtx, int **tomtx, int size);
+//int ** copyDMatrix(int **frommtx, int **tomtx, int size);
+//bool isNullTile(tile t);
 void usage();
 
 int main (int argc, char *argv[])
 {
-    int s = sizeof(int);
-
-    int n_mtxsize=16;
+    int n_mtxsize;
 
     if (argc != 2)
     {
@@ -49,24 +47,30 @@ int main (int argc, char *argv[])
 
     if (argc == 2)
     {
-        if(argv[1] == "-h") {
+        n_mtxsize = atoi(argv[1]);
+        if (n_mtxsize < B_TILE_SIZE) {
             usage();
-        }
-        else {
-           n_mtxsize = atoi(argv[1]);
         }
     }
 
     printf("Running with size matrix = %d\n", n_mtxsize);
     printf("Tiled version with B tile size = %d\n\n", B_TILE_SIZE);
 
-    int ** D_matrix;
-    //int D_matrix[MATRIX_SIZE][MATRIX_SIZE];
+    int ** D_matrix = malloc(n_mtxsize*sizeof(int*));
+    //mtx = aligned_alloc(16, n_mtxsize*sizeof(int*));
 
-    D_matrix=initMatrix(D_matrix, n_mtxsize);
-    printf("Init the marix done\n");
+    int ** D_matrix_nextk = malloc(n_mtxsize*sizeof(int*));
+    //mtx = aligned_alloc(16, n_mtxsize*sizeof(int*));
+
+    D_matrix = initMatrix(D_matrix, n_mtxsize);
+    printf("Init the matrix done\n");
 
     prettyPrintMatrix(D_matrix, n_mtxsize);
+
+    printf("Init the next k+1 matrix\n");
+    D_matrix_nextk = initNextMatrix(D_matrix_nextk, n_mtxsize, D_matrix);
+    printf("Init the next k+1 matrix done. Here it is:\n");
+    prettyPrintMatrix(D_matrix_nextk, n_mtxsize);
 
     floydWarshallTiled(D_matrix, n_mtxsize);
     printf("floydWarshallTiled done\n");
@@ -75,6 +79,8 @@ int main (int argc, char *argv[])
 
     printf("Freeing resources\n");
     free(D_matrix);
+    free(D_matrix_nextk);
+    printf("Done\n");
 
     return 0;
 }
@@ -82,35 +88,27 @@ int main (int argc, char *argv[])
 void floydWarshallTiled(int **mtx, int size) {
     // In k=0, we set the edge costs
     // n step B: skip every B-tile size
-    for (int k=1; k<size; k=k+B_TILE_SIZE) {
+    for (int k=0; k<size; k=k+B_TILE_SIZE) {
         printf("Now on k = %d\n", k);
         // CR tile
-        tile cr_tile = {
+        /*tile cr_tile = {
             .id = 1,
             .offset_i = 0,  // B_TILE_SIZE
             .offset_j = 0   // B_TILE_SIZE
-        };
-        floydWarshallCore(mtx, B_TILE_SIZE, cr_tile);
+        };*/
+        
+        floydWarshallTileCore(mtx, size, k);
+        //floydWarshallCore(mtx, B_TILE_SIZE, cr_tile);
 
-        // E, W, N, S tiles
-        tile t = getEastTile(cr_tile, size);
-        if (!isNullTile(t)) {
-            floydWarshallCore(mtx, B_TILE_SIZE, t);
-        }
-
-        t = getWestTile(cr_tile, size);
-        if (!isNullTile(t)) {
-            floydWarshallCore(mtx, B_TILE_SIZE, t);
-        }
-
-        t = getNorthTile(cr_tile, size);
-        if (!isNullTile(t)) {
-            floydWarshallCore(mtx, B_TILE_SIZE, t);
-        }
-
-        t = getSouthTile(cr_tile, size);
-        if (!isNullTile(t)) {
-            floydWarshallCore(mtx, B_TILE_SIZE, t);
+        // TODO: E, W, N, S tiles
+        // Is it better to collect tiles in array or like below? For parallelize?
+        int t_base_col_j = floor(k / size)*size;
+        int t_base_row_i = floor(k % B_TILE_SIZE)-1;
+        for (int t=t_base_col_j; t_base_col_j<t_base_col_j+B_TILE_SIZE; t=t+B_TILE_SIZE) {
+            // These iterates over E, W tiles
+            // Skip cr tile
+            if (t==k) continue;
+            printf("Would iterate over tiles with k %d\n", t);
         }
 
         // TODO: NE, NW, SE, SW tiles
@@ -118,28 +116,21 @@ void floydWarshallTiled(int **mtx, int size) {
     }
 }
 
-void floydWarshallCore(int **mtx, int size, tile t) {
-    // size = tile size (not matrix size)
-    // todo: This needs to operate on just the tile block of the matrix
-    // size is now the block tile size, not the matrix size
-    int maxtile_j = t.offset_j+size;
-    int maxtile_i = t.offset_i+size;
-    for (int k=1; k<size; k++) {
-        printf("  floydWarshallCore now k = %d\n", k);
-        for(int j=t.offset_j; j<maxtile_j; ++j) {
-            for(int i=t.offset_i; i<maxtile_i; ++i) {
-                // Set mtx[i][j] for k+1 to the minimum
-                // can skip where i=j
-                if (i==j)  continue;
+void floydWarshallTileCore(int **mtx, int size, int k) {
+    printf("  floydWarshallCore now k = %d\n", k);
+    for(int j=0; j<size; ++j) {
+        for(int i=0; i<size; ++i) {
+            // Set mtx[i][j] for k+1 to the minimum
+            // can skip where i=j
+            if (i==j)  continue;
 
-                int tmpcost = mtx[i][k] + mtx[k][j];
-                if (tmpcost < mtx[i][j]) {
-                    printf("Changing cost from %d to %d\n", mtx[i][k], tmpcost);
-                    mtx[i][j] = tmpcost;
-                }
-                else {
-                    printf("Keeping current cost at %d\n", mtx[i][j]);
-                }
+            int tmpcost = mtx[i][k] + mtx[k][j];
+            if (tmpcost < mtx[i][j]) {
+                printf("Changing cost from %d to %d\n", mtx[i][j], tmpcost);
+                mtx[i][j] = tmpcost;
+            }
+            else {
+                printf("Keeping current cost at %d\n", mtx[i][j]);
             }
         }
     }
@@ -169,81 +160,20 @@ void floydWarshallStandard(int **mtx, int size) {
     }
 }
 
-tile getEastTile(tile cr, int size) {
-    // Get the east tile if there is one
-    if(cr.offset_j+B_TILE_SIZE < size) {
-        tile t = {
-            //id=
-            .offset_i = cr.offset_i,
-            .offset_j = cr.offset_j+B_TILE_SIZE
-        };
-        return t;
-    }
-
-    return NULL_TILE;
-}
-tile getWestTile(tile cr, int size) {
-    // Get the west tile if there is one
-    if(cr.offset_j > B_TILE_SIZE) {
-        tile t = {
-            //id=
-            .offset_i = cr.offset_i,
-            .offset_j = cr.offset_j-B_TILE_SIZE
-        };
-        return t;
-    }
-
-    return NULL_TILE;
-}
-tile getNorthTile(tile cr, int size) {
-    // Get the north tile if there is one
-    if(cr.offset_i > B_TILE_SIZE) {
-        tile t = {
-            //id=
-            .offset_i = cr.offset_i - B_TILE_SIZE,
-            .offset_j = cr.offset_j
-        };
-        return t;
-    }
-
-    return NULL_TILE;
-}
-tile getSouthTile(tile cr, int size) {
-    // Get the south tile if there is one
-    if(cr.offset_i+B_TILE_SIZE<size) {
-        tile t = {
-            //id=
-            .offset_i = cr.offset_i + B_TILE_SIZE,
-            .offset_j = cr.offset_j
-        };
-        return t;
-    }
-
-    return NULL_TILE;
-}
-
-void printTile(tile cr) {
-    printf("Tile id %d, offset_i %d, offset_j %d\n", cr.id, cr.offset_i, cr.offset_j);
-}
-
 int ** initMatrix(int **mtx, int size) {
   int seed = time(NULL);
   srand(seed);
 
-  mtx = aligned_alloc(16, size*sizeof(int*));
-  //mtx = malloc(size*sizeof(int*));
-
   for(int r=0; r<size; r++)
   {
-    mtx[r] = aligned_alloc(16, size*sizeof(int*));
-    //mtx[r] = malloc(size*sizeof(int));
+    //mtx[r] = aligned_alloc(16, size*sizeof(int*));
+    mtx[r] = malloc(size*sizeof(int));
   }
   printf("After malloc row\n");
 
   // Set the random values
   for(int r=0; r<size; r++)
   {
-    printf("In row loop, setting %d \n", r);
     for(int c=0; c<size; c++)
     {
         if (r==c) {
@@ -263,27 +193,52 @@ int ** initMatrix(int **mtx, int size) {
   }
   return mtx;
 }
-void prettyPrintMatrix(int **mtx, int size) {
+
+int ** initNextMatrix(int **mtx, int size, int **frommtx) {
+  // Initializes matrix mtx with values from frommtx
+  for(int r=0; r<size; r++)
+  {
+    //mtx[r] = aligned_alloc(16, size*sizeof(int*));
+    mtx[r] = malloc(size*sizeof(int));
+  }
+  copyMatrixValues(frommtx, mtx, size);
+  //copyDMatrix(frommtx, mtx, size);
+  return mtx;
+}
+
+void copyMatrixValues(int **frommtx, int **tomtx, int size)
+{
+    // Copies the values of frommtx to tomtx
+    for (int r=0; r<size; r++) {
+        for (int c=0; c<size; c++) {
+            tomtx[r][c] = frommtx[r][c];
+        }
+    }
+}
+
+/*int ** copyDMatrix(int **frommtx, int **tomtx, int size) {
+    // Copies the values of frommtx to tomtx
+    int b = sizeof(int) * size * size;
+    frommtx = memcpy (tomtx, frommtx, b);
+    return frommtx;
+}*/
+
+void prettyPrintMatrix(int **mtx, int size)
+{
     for(int i=0; i<size; ++i) {
-        printf("\n\n");
         for(int j=0; j<size; ++j)
         {
             printf("%d  ", mtx[i][j]);
         }
+        printf("\n\n");
     }
     printf("\n");
-}
-bool isNullTile(tile t) {
-    if (t.id == -1) {
-        return true;
-    }
-    return false;
 }
 
 void usage()
 {
   //printf("Usage: %d N\n", program);
   //printf("  N: size of matrix\n");
-  printf("please supply arugment for size of matrix");
+  printf("please supply argument for size of matrix, greater than %d\n", B_TILE_SIZE);
   exit(1);
 }
